e765a6478ecbd3957d151b246e318152f20af482
Sabina Baholli <Emiliano.Dervishi@velociti.com>
2021-11-07 11:44:40 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    component: ToDoListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   onAddForm = (data: any) => {
     const email = data.form.value.email;
     const password = data.form.value.password;
+    this.router.navigate(['/list']);
   }
 
   ngOnInit(): void {
