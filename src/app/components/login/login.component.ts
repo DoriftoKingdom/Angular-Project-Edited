f64e0dada55e46113f835f43e0c211f8be4f8987
Sabina Baholli <Emiliano.Dervishi@velociti.com>
2021-11-13 15:51:29 +0100

    add login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,14 +10,21 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private beService: BackendService
   ) { }
 
   login = (data: any) => {
     const email = data.form.value.email;
     const password = data.form.value.password;
-    this.router.navigate(['/list']);
-    localStorage.setItem('isLoggedIn', 'true');
+    this.beService.getUsers().subscribe((users) => {
+      const user = users.find(user => user.email === email && user.password === password);
+      console.log(user);
+      if (user) {
+        this.router.navigate(['/list']);
+        localStorage.setItem('isLoggedIn', 'true');
+      }
+    })
   }
 
   ngOnInit(): void {
